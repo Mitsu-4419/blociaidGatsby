ac16f36cd23698c9a299dda0eb62aee2061efdf5
Mitsutomo Yamada <Mitsu44@Mitsutomo-no-MacBook-Air.local>
2023-04-26 22:05:47 +0900

    change rader chart

--- src/js/RadarChart.js
+++ src/js/RadarChart.js
@@ -16,22 +16,22 @@ export const RadarChart=memo((props)=> {
     useEffect(()=>{
         if(props["chartType"]==="front"&& window_w > 400){
             setOption(RadarOptions('Frontend',25))
-            setData(RadarDataBack(["Vue/nuxt","Quasar","Flutter","GraphQL", "WASM", "Typescript","React.js"], "rgba(34, 202, 236, .2)", "rgba(34, 202, 236, 1)", [5,5,4,3,2,4,5]))
+            setData(RadarDataBack(["Vue.js","Quasar","Flutter","GraphQL", "WASM", "Typescript","React.js"], "rgba(34, 202, 236, .2)", "rgba(34, 202, 236, 1)", [5,5,4,3,2,4,5]))
         }else if(props["chartType"]==="front"&& window_w <= 400){
             setOption(RadarOptions('Frontend',18))
-            setData(RadarDataBack(["Vue/nuxt","Quasar","Flutter","GraphQL", "WASM", "Typescript","React.js"], "rgba(34, 202, 236, .2)", "rgba(34, 202, 236, 1)", [5,5,4,3,2,4,4]))
+            setData(RadarDataBack(["Vue.js","Quasar","Flutter","GraphQL", "WASM", "Typescript","React.js"], "rgba(34, 202, 236, .2)", "rgba(34, 202, 236, 1)", [5,5,4,3,2,4,4]))
         }else if (props["chartType"]==="backend" && window_w > 400){
             setOption(RadarOptions('Backend',25))
-            setData(RadarDataBack(["Node.js", "Python", "Django","C,C++", "GCP", "Firebase","Solidity"], "rgba(34, 236, 88, .2)", "rgba(34, 236, 88, 1)", [4,5,4,2,4,5,3]))
+            setData(RadarDataBack(["Node.js", "Python", "Django","Rust", "GCP", "Firebase","Solidity"], "rgba(34, 236, 88, .2)", "rgba(34, 236, 88, 1)", [4,5,4,2,4,5,3]))
         }else if(props["chartType"]==="backend" && window_w <= 400){
             setOption(RadarOptions('Backend',18))
-            setData(RadarDataBack(["Node.js", "Python", "Django","C,C++", "GCP", "Firebase","Solidity"], "rgba(34, 236, 88, .2)", "rgba(34, 236, 88, 1)", [4,5,4,2,4,5,3]))
+            setData(RadarDataBack(["Node.js", "Python", "Django","Rust", "GCP", "Firebase","Solidity"], "rgba(34, 236, 88, .2)", "rgba(34, 236, 88, 1)", [4,5,4,2,4,5,3]))
         }else if(props["chartType"]==="others" && window_w > 400){
             setOption(RadarOptions('Others',25))
-            setData(RadarDataBack(["Medical", "Statistics", "Docker","AI", "DataScience","BlockChain"], "rgba(236, 68, 34, .2)","rgba(236, 68, 34, 1)", [5,4,3,3,4,3]))
+            setData(RadarDataBack(["Medical", "Statistics", "Docker","AI", "DataScience","BlockChain"], "rgba(236, 68, 34, .2)","rgba(236, 68, 34, 1)", [5,4,3,4,4,3]))
         }else if(props["chartType"]==="others" && window_w <= 400){
             setOption(RadarOptions('Others',18))
-            setData(RadarDataBack(["Medical", "Statistics", "Docker","AI", "DataScience","BlockChain"], "rgba(236, 68, 34, .2)","rgba(236, 68, 34, 1)", [5,4,3,3,4,3]))
+            setData(RadarDataBack(["Medical", "Statistics", "Docker","AI", "DataScience","BlockChain"], "rgba(236, 68, 34, .2)","rgba(236, 68, 34, 1)", [5,4,3,4,4,3]))
         }
     },[])
     return (
